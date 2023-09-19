241bbb2390a5842ea8673f708d6af653ab9082b1
IAmMoltony <koronavirusnyj@gmail.com>
2023-09-19 16:06:51 +0600

    Add sync script

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cpcfg() {
+    echo "Syncing $1"
+    cp $HOME/$1 .
+}
+
+cpcfg .bashrc
+cpcfg .gitconfig
+cpcfg .profile
+cpcfg .vimrc
+
+echo "Syncing melonDS config"
+cp $HOME/.config/melonDS/melonDS.ini
+
+echo "Syncing Xfce config"
+cp $HOME/.config/xfce4 . -r
+rm -r xfce4/xfce4-screenshooter xfce4/desktop xfce4/panel
+
+echo "Success"
