d811758512acb3787d067ff67e85d6ab6c087969
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-05 14:28:38 +0500

    Better cascadia code installer

--- install-cascadia-code.sh
+++ install-cascadia-code.sh
@@ -2,26 +2,77 @@
 
 set -e
 
+DLCC_Wget() {
+    wget https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
+
+    if [ "$?" != "0" ]; then
+        # non-zero error code means bad
+        echo "Failed to download cascadia code!!!! try again next time and make sure your internet workd!!!!"
+        exit 1
+    fi
+}
+
+DLCC_Curl() {
+    curl -O https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
+
+    if [ "$?" != "0" ]; then
+        # non-zero error code means bad
+        echo "failed to download cascadia code!!!!!! Try again next time and make sure that your wifi is plugged in"
+        exit 1
+    fi
+}
+
+check_if_cascadia_code_is_installed_already() {
+    if fc-list | grep -q "Cascadia Code"; then
+        echo "Yo cascadia code is already installed you vibing fr!!!"
+        echo "If it doesn't work then delete it and run this script again"
+        exit
+    fi
+}
+
+check_if_cascadia_code_is_installed_already
+
 echo "INSTALLING CASCADIA CODE!!!"
-mkdir _Tmp
+
+# Create and cd into a temp folder
+mkdir -p _Tmp
 cd _Tmp
 
 echo "Downloading cascadia code ..."
-wget https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
+if command -v wget > /dev/null 2>&1; then
+    # Wget found so try using it
+    DLCC_Wget
+else
+    # Wget not found, try curl
+    if command -v curl > /dev/null 2>&1; then
+        DLCC_Curl
+    else
+        # neither were found
+        echo "Neither curl nor wget are found"
+        echo "Please try again after installing curl or wget"
+        exit 1
+    fi
+fi
 
+# Extract the font
 echo "Extracting cascadia code ..."
 unzip CascadiaCode-2111.01.zip
 
+# install it
+# This installs system wide, I might want to add an option for installing for one user
+# But I don't wanna since installing systmem wide works for me and that's all I really care about
 echo "Installing fonts ..."
-echo "You might have to enter your password"
+echo "You might have to enter your password cuz it's installing system wide"
 sudo mkdir /usr/share/fonts/cascadiaCode
 sudo mv ./otf /usr/share/fonts/cascadiaCode/otf
 sudo mv ./ttf /usr/share/fonts/cascadiaCode/ttf
 sudo mv ./woff2 /usr/share/fonts/cascadiaCode/woff2
 
+# Update font cache so the user doesn't have to
 echo "Updating font-cache ..."
 fc-cache -fv
 
+# Pai pai temp folder
 echo "Cleaning up ..."
 cd ..
 rm -rf _Tmp
