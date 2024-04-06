151ebe56a39b21f8debd0f512e45d9b96160d20c
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-06 10:36:31 +0500

    Cron sync update

--- cronsync.sh
+++ cronsync.sh
@@ -4,3 +4,13 @@ notify-send "Running hourly config sync."
 cd $HOME/configs # Just to be safe
 mkdir -p HourlySyncLogs
 ./sync-commit.sh "Automatic hourly sync: $(date "+%F %T")" > HourlySyncLogs/hsl_$(date "+%F_%T")
+
+if [ "$?" == "0" ];
+    notify-send "HCS: all good"
+fi
+if [ "$?" == "1" ]; then
+    notify-send "HCS: can't push"
+fi
+if [ "$?" == "2" ]; then
+    notify-send "HCS: no changes"
+fi
