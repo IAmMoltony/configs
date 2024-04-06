1d64026faa357c4c11df6da93ae5178225a26fed
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-06 18:02:26 +0500

    Fix cronsync

--- cronsync.sh
+++ cronsync.sh
@@ -4,9 +4,8 @@ notify-send "Running hourly config sync."
 cd $HOME/configs # Just to be safe
 mkdir -p HourlySyncLogs
 ./sync-commit.sh "Automatic hourly sync: $(date "+%F %T")" > HourlySyncLogs/hsl_$(date "+%F_%T")
-echo "dbg"
 
-if [ "$?" == "0" ];
+if [ "$?" == "0" ]; then
     notify-send "HCS: all good"
 fi
 if [ "$?" == "1" ]; then
