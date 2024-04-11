2de6838db098a3c2f13373038a7390b37bba5e64
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-11 16:50:07 +0500

    Update cron sync

--- cronsync.sh
+++ cronsync.sh
@@ -5,14 +5,4 @@ cd $HOME/configs # Just to be safe
 mkdir -p HourlySyncLogs
 ./sync-commit.sh "Automatic hourly sync: $(date "+%F %T")" > HourlySyncLogs/hsl_$(date "+%F_%T")
 
-echo "dbg: Exit code = $?"
-
-if [ "$?" == "0" ]; then
-    notify-send "HCS: all good"
-fi
-if [ "$?" == "1" ]; then
-    notify-send "HCS: can't push"
-fi
-if [ "$?" == "2" ]; then
-    notify-send "HCS: no changes"
-fi
+notify-send "Sync done with exit code $?"
