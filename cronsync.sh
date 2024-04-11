2574ae20844e6bfd135a916980bcf3c4f2fba4b6
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-11 17:23:21 +0500

    Add disabling hsl

--- cronsync.sh
+++ cronsync.sh
@@ -2,6 +2,12 @@
 
 notify-send "Running hourly config sync."
 cd $HOME/configs # Just to be safe
+
+if [[ -f ./HSL_IS_DISABLED ]]; then
+    notify-send "Sync is disabled, halting"
+    exit
+fi
+
 mkdir -p HourlySyncLogs
 ./sync-commit.sh "Automatic hourly sync: $(date "+%F %T")" > HourlySyncLogs/hsl_$(date "+%F_%T") 2>&1
 
