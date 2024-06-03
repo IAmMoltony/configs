a8a719111d054a7d8bdd3b8bb25ea07d68865ac1
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-03 16:58:35 +0500

    oblogout installer version 1 point 1

--- install-oblogout.sh
+++ install-oblogout.sh
@@ -9,7 +9,7 @@ selfdestruct() {
 }
 
 echo "o hi welcom to OBLOGOUT INSTALLER"
-echo "this is version 1 point 0 of the installer (not the program)"
+echo "this is version 1 point 1 of the installer (not the program)"
 echo "instal oblogout??? (y or n)"
 read -p "???? " -n 1 -r
 echo
@@ -17,10 +17,14 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "IM INSTALLIG OBLOGOUT RIGHT NOW!!!!!!!"
 
-    echo "STEP 1: clone oblogout"
-    echo "it will be cloned in your home folder"
-    git clone https://github.com/trizen/oblogout-py3 $HOME/oblogout-py3 || selfdestruct
-    echo "ok i cloned it"
+    if [ ! -d "$HOME/oblogout-py3" ]; then
+        echo "STEP 1: clone oblogout"
+        echo "it will be cloned in your home folder"
+        git clone https://github.com/trizen/oblogout-py3 $HOME/oblogout-py3 || selfdestruct
+        echo "ok i cloned it"
+    else
+        echo "attention rightn ow: oblogout has already been cloned"
+    fi
 
     echo "STEP 2: instaling"
     cd $HOME/oblogout-py3
