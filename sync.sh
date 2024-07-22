cf6eb2737500227f3013292895e3a9f9f97ce5e3
IAmMoltony <koronavirusnyj@gmail.com>
2024-07-22 16:41:43 +0500

    Use cpcfg instead of cpcfgf to copy mpv.conf

--- sync.sh
+++ sync.sh
@@ -23,11 +23,11 @@ cpcfg .conkyrc
 cpcfg .config/melonDS/melonDS.ini
 cpcfg .config/compton/compton.conf
 cpcfg .config/bleachbit/bleachbit.ini
+cpcfg .config/mpv/mpv.conf
 cpcfgf .config/qutebrowser/config.py qutebrowser.py
 cpcfgf .config/i3/config i3cfg
 cpcfgf .config/openbox/rc.xml obrc.xml
 cpcfgf .config/openbox/autostart.sh obautostart.sh
-cpcfgf .config/mpv/mpv.conf mpv.conf
 
 echo "Success"
 
