57934e0f0be47a9dc8241f759e7d317993bcfc98
IAmMoltony <koronavirusnyj@gmail.com>
2025-03-25 21:03:26 +0500

    Sync: I am kasane teto

--- module/aliases/manual/sync.sh
+++ module/aliases/manual/sync.sh
@@ -7,3 +7,4 @@ alias syncpaipai='synccfgs && paipai' # Sync configs and say pai pai
 alias spaipai='syncpaipai' # Today's installment of Useless Aliases... Short version of `syncpaipai' !!!
 alias scfgsm='synccfgsm' # Short versoin alias thing ojgjo2joejfjojii243jrjoo2 2323oin23roin23r
 alias syncmrbabf='syncmrbfba' # Same as above but like (bf needs to be source before ba so same)
+alias syncm='synccfgsm' # テテテテトテト テテテテトリス
