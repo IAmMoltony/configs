db71f2b15fd920b554ecd4282358914e37ac25e1
IAmMoltony <koronavirusnyj@gmail.com>
2025-04-14 20:06:52 +0500

    load insults from file

--- random-insult.sh
+++ random-insult.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/true
 
-# TODO implement dynamic insult list loaded from file
 random_insult() {
-    local insults=("big doofus" "disappointment" "failure" "garbage truck" "stupid" "cabbagehead" "silly goose" "clown" "potato brain" "cirno" "fricking lunatic" "drongo")
+    mapfile -t insults < "$(dirname $0)"/insults.txt
     echo "${insults[RANDOM % ${#insults[@]}]}"
 }
