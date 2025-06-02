61487a9c1d07b0d2fe38d8bc630b9ab79a18760a
IAmMoltony <koronavirusnyj@gmail.com>
2025-06-02 10:38:11 +0000

    Sync: add eselect news read alisa

--- module/aliases/manual/pkg/emerge.sh
+++ module/aliases/manual/pkg/emerge.sh
@@ -3,3 +3,4 @@
 alias sem='sudo emerge' # emerges casually
 alias sema='sudo emerge --ask' # emerges; asks
 alias semacudw='sudo emerge --ask --changed-use --deep @world' # emerges; asks; changes USE; digs deep in the @world
+alias sesnr='sudo eselect news read' # read the daily newspaper
