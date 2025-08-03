8b7a82773c6b528f7e9bd07812602fef6590bc44
IAmMoltony <koronavirusnyj@gmail.com>
2025-08-03 21:21:28 +0500

    Sync: updating the system updating the system

--- module/aliases/manual/pkg/emerge.sh
+++ module/aliases/manual/pkg/emerge.sh
@@ -4,3 +4,4 @@ alias sem='sudo emerge' # emerges casually
 alias sema='sudo emerge --ask' # emerges; asks
 alias semacudw='sudo emerge --ask --changed-use --deep @world' # emerges; asks; changes USE; digs deep in the @world
 alias sesnr='sudo eselect news read' # read the daily newspaper
+alias sesasemacudw='sudo emaint sync -a && semacudw' # tl;dr update system
